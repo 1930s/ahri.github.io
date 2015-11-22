b295647ae2abb0d3865951a3c8a9372471215170
Adam Piper <adam@ahri.net>
2015-11-22 18:07:58 +0000

    Improve new.sh helper script.

--- new.sh
+++ new.sh
@@ -2,29 +2,129 @@
 
 set -ue
 
-if [ $# -lt 1 ]; then
-	echo "Provide a title" 1>&2
-	exit 1
-fi
+usage()
+{
+    echo
+    echo "`basename "$0"` \"some title\""
+    echo
+    echo "    --tags=tag1,tag2"
+    echo "    --category=cat"
+    echo
+    echo "Existing categories:" "`grep '^categories:' -r content/post/ | normalize`"
+    echo "Existing tags:" "`grep '^tags: ' -r content/post/ | normalize`"
+}
+
+error()
+{
+    fmt="$1"
+    shift
+    printf "ERROR: $fmt\n" "$@" >&2
+}
+
+escape()
+{
+    sed 's,[^a-zA-Z0-9 ],,g;s, ,-,g;s,-\+,-,g' | tr "[:upper:]" "[:lower:]"
+}
 
 normalize()
 {
-	sed 's,[^a-zA-Z0-9 ],,g;s, ,-,g;s,-\+,-,g' | tr "[:upper:]" "[:lower:]"
+    awk -F',' "
+    {
+        sub(/^.*[:=] */, \"\");
+        sub(/^\[/, \"\");
+        sub(/\]$/, \"\");
+        sub(/^\"/, \"\");
+        sub(/\"$/, \"\");
+        gsub(/\", *\"/, \"\");
+    }
+
+    NF > 0 {
+        for (i=1; i<=NF; i++) {
+            print \$i;
+        }
+    }" | tr '[:upper:]' '[:lower:]' | sort -u | tr "\n" "," | sed 's/,$//;s/,/, /g'
 }
 
-binary()
+expand_list()
 {
-	if [ "$OS" = "Windows_NT" ]; then
-		echo "hugo/windows.exe"
-		return
-	fi
+    read comma_separated
+    if [ "$comma_separated" = "" ]; then
+        echo "[]"
+        return
+    fi
 
-	echo "ERROR: Unknown OS" 1>&2
-	exit 1
+    echo "[\"$comma_separated\"]" | sed 's/,/", "/g'
 }
 
-title="`echo "$1" | normalize`"
-title="$1"
-binary=`binary`
+set_once()
+{
+    varname=$1
+    value=$2
 
-$binary new "post/`date +%Y-%m-%d`-$title".md
+    read -r -d '' check <<EOF || true
+        if [ ! -z "\$$varname" ]; then
+            error "$varname specified twice: \"%s\", then \"%s\"" "\$$varname" "\$value"
+            exit 1
+        fi
+        $varname="\$value"
+EOF
+
+    eval "$check"
+}
+
+if [ $# -lt 1 ]; then
+    usage
+    exit 1
+fi
+
+title=""
+tags=""
+category=""
+while [ $# -gt 0 ]; do
+    param=`echo $1 | awk -F= '{print $1}'`
+    value=`echo $1 | awk -F= '{print $2}'`
+    case $param in
+        -h | --help)
+            usage
+            exit 1
+            ;;
+        --tags)
+            set_once tags "$value"
+            ;;
+        --category)
+            set_once category "$value"
+            ;;
+        --*)
+            error "unknown parameter %s" "$param"
+            usage
+            exit 1
+            ;;
+        *)
+            set_once title "$param"
+            ;;
+    esac
+    shift
+done
+
+title="`echo "$title" | sed 's,",\\\\",g'`"
+escaped_title="`echo "$title" | escape`"
+
+if [ -z "$category" ]; then
+    category="software development"
+fi
+
+date=`date -u +%Y-%m-%d`
+datetime=`date -u +%Y-%m-%dT%H:%M:%SZ`
+
+filename="content/post/$date-$escaped_title".md
+cat <<EOF > "$filename"
+---
+title: "$title"
+description: ""
+date: "$datetime"
+categories: ["$category"]
+tags: `echo $tags | expand_list`
+---
+EOF
+
+echo "Created: $filename"
